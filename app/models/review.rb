97fa6d312f9037bc74928568e3115cc38ec6547a
Miguel Jimenez <denkatalabeno@gmail.com>
2017-06-07 13:08:24 +0200

    done

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,4 @@ class Review < ApplicationRecord
   validates :content, presence: true
   validates :rating, presence: true
   validates :rating, inclusion: { in: (0..5) }, numericality: { only_integer: true }
-
-
 end
