ac03662e7a74e489350dad024258039a7390dab4
Miguel Jimenez <denkatalabeno@gmail.com>
2017-06-07 11:51:02 +0200

    models fully developed

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,9 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :address, presence: true
+  validates :category, acceptance: { accept: ['french', 'chinese', 'japanese', 'italian', 'belgian'] }
+
 end
